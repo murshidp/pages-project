1d3c2eb695dc7f5892c7c921efd4e82f8ab6ab35
murshidp <murshidparambadan@gmail.com>
2020-01-04 07:02:19 +0530

    initial commit

---
+++ pages_project/pages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
+# pages/views.py
+
+
+
+
+
+class HomePageView(TemplateView):
+    template_name='home.html'
+
+class AboutPageView(TemplateView):
+    template_name="about.html"
+    
